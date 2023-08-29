ab7251f6deb42eb661d4644b71560a9dbef0b542
DIO <ANDREW@LAPTOP-2OUNVGRJ>
2023-08-30 06:36:52 +0800

    added accountInfo

---
+++ PHPay/src/PHPay/SQLQuery.java
@@ -0,0 +1,53 @@
+package PHPay;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SQLQuery {
+	private static SQLConnection con;
+
+
+	public static double getBalance(String ID) {
+		SQLConnection con = new SQLConnection();
+		double bal = 0.0;
+		String query = "SELECT `balance` FROM `users` WHERE id = ?";
+		try {
+			PreparedStatement ps = con.getCon().prepareStatement(query);
+			ps.setString(1, ID);
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				bal = rs.getDouble("balance");
+				System.out.println(bal);
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(userDATA.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return bal;
+	}
+	
+	public boolean IDExists(String ID) {
+		SQLConnection con = new SQLConnection();
+		
+		String query = "SELECT `id` FROM `users`";
+		try {
+			Statement stmt = con.getCon().createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			while (rs.next()) {
+				String checkID = rs.getString("id");
+				if (checkID.equals(ID)) {
+					return true;
+				}
+ 			}
+		} catch (SQLException ex) {
+			Logger.getLogger(userDATA.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return false;
+	}
+
+}
---
+++ PHPay/src/PHPay/phpayMain.java
@@ -0,0 +1,17 @@
+package PHPay;
+
+import javax.swing.JFrame;
+
+public class phpayMain extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		loadingPHP hello = new loadingPHP();
+		hello.setVisible(true);
+	}
+
+}
