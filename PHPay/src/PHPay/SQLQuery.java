ee23c0f7de51a1fb82c84bdbc9aed5c53f4d72d3
DIO <DIO@LAPTOP-2OUNVGRJ>
2023-08-31 12:14:57 +0800

    added addBalance function on SQLQuery

--- PHPay/src/PHPay/SQLQuery.java
+++ PHPay/src/PHPay/SQLQuery.java
@@ -10,7 +10,6 @@ import java.util.logging.Logger;
 public class SQLQuery {
 	private static SQLConnection con;
 
-
 	public static double getBalance(String ID) {
 		SQLConnection con = new SQLConnection();
 		double bal = 0.0;
@@ -29,25 +28,41 @@ public class SQLQuery {
 		}
 		return bal;
 	}
-	
+
 	public boolean IDExists(String ID) {
 		SQLConnection con = new SQLConnection();
-		
+
 		String query = "SELECT `id` FROM `users`";
 		try {
 			Statement stmt = con.getCon().createStatement();
 			ResultSet rs = stmt.executeQuery(query);
-			
+
 			while (rs.next()) {
 				String checkID = rs.getString("id");
 				if (checkID.equals(ID)) {
 					return true;
 				}
- 			}
+			}
 		} catch (SQLException ex) {
 			Logger.getLogger(userDATA.class.getName()).log(Level.SEVERE, null, ex);
 		}
 		return false;
+	}
+
+	public static void addBalance(String ID, double addCash) {
+		SQLConnection con = new SQLConnection();
+
+		String query = "UPDATE `users` SET `balance` = balance + ? WHERE id = ?";
+
+		try {
+			PreparedStatement ps = con.getCon().prepareStatement(query);
+			ps.setDouble(1, addCash);
+			ps.setString(2, ID);
+			ps.executeUpdate();
+
+		} catch (SQLException ex) {
+			Logger.getLogger(userDATA.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 
 }
