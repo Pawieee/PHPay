f416378aff71d7f259957649dfa0ac2c2bf7fb32
JACK STAR <Administrator@JACK>
2023-09-14 22:28:27 +0800

    jacksept14

---
+++ PHPay/src/PHPay/RoundedPasswordField.java
@@ -0,0 +1,38 @@
+package PHPay;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundedPasswordField extends JPasswordField {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Shape shape;
+
+    public RoundedPasswordField(int columns) {
+        super(columns);
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (!isOpaque()) {
+            super.paintComponent(g);
+            return;
+        }
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        if (shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 15, 15); // Adjust the radius as needed
+        }
+
+        g2d.setColor(getBackground());
+        g2d.fill(shape);
+
+        super.paintComponent(g);
+    }
+}
