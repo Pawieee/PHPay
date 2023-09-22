145fdd9cb03e8499b4443a43f097f53dc3332a75
Rafael <Rafael@192.168.0.27>
2023-09-22 22:58:27 +0800

    hi

---
+++ PHPay/src/PHPay/HelpPanel.java
@@ -0,0 +1,19 @@
+package PHPay;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class HelpPanel extends JPanel {
+	public HelpPanel(String ID) {
+		
+		
+		setBounds(0, 0, 1200, 700);
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(61, 88, 333, 245);
+		add(lblNewLabel);
+
+		
+	}
+}
