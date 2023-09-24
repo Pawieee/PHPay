dbd6f1fdda602d3a7117046d5c5f8053529d0a69
Pawieee <p.agujitas.534827@umindanao.edu.ph>
2023-09-24 16:08:28 +0800

    wala lang

--- PHPay/src/PHPay/SQLConnection.java
+++ PHPay/src/PHPay/SQLConnection.java
@@ -14,7 +14,7 @@ public class SQLConnection {
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			setCon("jdbc:mysql://localhost:3306/PHPay", "root", "4304Qwe123");
+			setCon("jdbc:mysql://localhost:3306/PHPay", "root", "password");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
