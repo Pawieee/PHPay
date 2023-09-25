68f53341590ae283386d97f678a29585f79f6fd9
Vain <Vain@DESKTOP-O92F8SP>
2023-09-25 17:46:02 +0800

    123123

---
+++ PHPay/src/PHPay/HelpPanel1.java
@@ -0,0 +1,12 @@
+package PHPay;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Window.Type;
+
+public class HelpPanel1 extends JFrame {
+    public HelpPanel1() {
+     
+    }
+}
